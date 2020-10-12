5b52e36fc56c1467cc87e12513eb742caffc2790
Christina Kaady <christina.kaady@gmail.com>
2020-10-12 15:12:16 -0700

    items added to dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,29 @@
-// pull in data
-d3.json('../data/samples.json').then(data => {
-
-    let subjectID = data.names
-    let metadata = data.metadata
-    let samples = data.samples;
-
-    console.log(data);
+// init function
+function init() {
     
-})
+    // Use the D3 library to read in samples.json and add to dropdown
+    d3.json('../data/samples.json').then(data => {
+
+        let subjectID = data.names
+        let metadata = data.metadata
+        let samples = data.samples
+
+        console.log(data);
+    
+    // Use D3 to select the dropdown menu
+    var dropdownMenu = d3.select("#selDataset")
+    .selectAll("select")
+    .data(subjectID)
+    .enter()
+    .append("option")
+    .attr("value", d => d)
+    .text(d => d);
+
+
+    })
+
+    }
+  
+init();
+  
+
